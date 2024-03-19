b46e37460ba7130841af2a217746dff9d0dce3df
Ayibiowu Gbenga <ayibiowug@gmail.com>
2024-03-19 14:00:55 +0100

    Fickle Flight

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const departure = document.getElementById ("departure")
+const arrivalInput = document.getElementById ("arrival")
+const dateInput = document.getElementById ("date")
+
+console.log(departure);
